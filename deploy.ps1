37f02906ee69e8b9b1ad1ed518d9afddb6cec7a5
Alfred Mathew <alfredmathew@outlook.com>
2020-07-11 19:54:27 -0400

    file rearrangement

---
+++ deploy.ps1
@@ -0,0 +1,8 @@
+﻿## Script to deploy project
+
+function get-deployed{
+    $env:FLASK_APP=".\src\flask_app.py"
+    flask run
+}
+
+get-deployed
--- src/flask_app.py
+++ src/flask_app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask import render_template
 from get_data import *
-app = Flask(__name__)
+
+
+app = Flask(__name__, template_folder="../templates")
 
 @app.route('/')
 def hello_world():
-    return "First value is " + get_data()['Facility_Name'][0]
+    return render_template('index.html')
