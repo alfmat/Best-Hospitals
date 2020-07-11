bca0400be9ef55725256a2aecb843770fcb165e0
Alfred Mathew <alfredmathew@outlook.com>
2020-07-10 23:26:20 -0400

    added automated commit file

---
+++ automated-commit.ps1
@@ -0,0 +1,16 @@
+﻿function get-menu{
+    clear-host
+    write-host "welcome to the automated git connector"
+    write-host "purpose of this script is to make it easier to commit to github"
+}
+
+function get-performcommit{
+    get-menu
+    $commit_msg = read-host 'What would you like commit message to be?'
+    git status
+    git add .
+    git commit -m $commit_msg
+    git push -u origin master
+}
+
+get-performcommit
