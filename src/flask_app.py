64257cebad90545c3f56748c1beca5d4445f1214
Alfred Mathew <alfredmathew@outlook.com>
2020-07-11 21:09:56 -0400

    dropped template and made my own

--- src/flask_app.py
+++ src/flask_app.py
@@ -3,7 +3,7 @@ from flask import render_template
 from get_data import *
 
 
-app = Flask(__name__, template_folder="../templates")
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 @app.route('/')
 def hello_world():
